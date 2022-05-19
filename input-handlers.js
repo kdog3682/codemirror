1087bbfe94535257392f64db0e4d6ecffb5b68c5
Kevin Lee <37029620+kdog3682@users.noreply.github.com>
2022-05-19 16:59:43 -0400

    --

--- input-handlers.js
+++ input-handlers.js
@@ -1,43 +1,19 @@
 const GlobalReferences = {
     js: {
-
         imapRef: jsImap,
         useLeader: true,
         magicLeader: true,
-        leaderCommands: {
-        },
-        iabCommands: {
-        },
-        iabSnippets: { // lastWordSnippets ... from the pos
-           map: 'map(($child) => {\n\t$c\n})',
-          'mab': "map(([a,b], i) => {\n\t$c\n})", 
-           filter: 'filter(($child) => {\n\t$c\n})',
-          'rab': ".reduce((acc, [a,b], i) => {\n\t$c\n}, {})", 
-          'red': ".reduce((acc, item, i) => {\n\t$c\n}, {})", 
-           iac: 'chicken',
-        },
-        treeCommands: {...baseTreeCommands, ...jsTreeCommands},
+        leaderCommands: {},
+        iabCommands: {},
     },
-    html: {treeCommands: baseTreeCommands,
-    
-        useLeader: true,
+    html: {
+        //useLeader: true,
     },
-    css: {treeCommands: {...baseTreeCommands, ...cssTreeCommands},
-    
-        useLeader: false,
+    css: {
+        //useLeader: false,
     },
 }
 
-const tabCommandRef = {
-    foo() {
-        console.log('hiiiiiiiiya')
-    },
-}
-
-const snippetOnTabRef = {
-    abc: 'HIII($last)',
-    ifs: 'if (isString($last)) {\n    $c\n}',
-}
 function htmlEnter(cm) {
     let f = cmPos(cm)
     const line = cm.state.doc.lineAt(f)
@@ -47,13 +23,13 @@ function htmlEnter(cm) {
         return 1
     }
     const [spaces, text] = getIndentAndLine(line.text)
+    //console.log([text, hasHtml(text)]); throw ''
     if (!text || hasHtml(text)) return
 
     const value = htmlLineParser(text)
     const output = indent(value, spaces, 'tabs')
     const [insert, cursor] = getCursorFromText(output)
     const anchor = line.from + cursor
-    //console.log({output, value, spaces, anchor})
     cmDispatchText(cm, line.from, line.to, insert, anchor)
     return 1
 }
@@ -62,7 +38,7 @@ function cssEnter(cm, k) {
     let f = cmPos(cm)
     const line = cm.state.doc.lineAt(f)
     const [spaces, text] = getIndentAndLine(line.text)
-    if (/[:;{}]/.test(text)) return
+    if (text.length < 3 || /[:;{}]/.test(text)) return
     if (spaces) {
         const dict = cssEvaluator(text)
         const cssValue = cssReduce(dict).trim()
@@ -87,28 +63,45 @@ function htmlShiftEnter(cm) {
     cmReplace(cm, cmLine, htmlLineFix)
 }
 
-function cssTab(cm) {
-    console.log('csstab')
+function normalHandler(s, enter) {
+    if (!enter) {
+        let ref = NormalRef.global[s] || NormalRef[e.lang][s]
+        if (ref) {
+            ref()
+            return true
+        }
+        return
+    }
+    let [key, arg] = splitonce(s)
+    let ref = NormalRef.global[key]
+    if (!ref) {
+        ref = NormalRef[app.editor.lang][key]
+    }
+    if (ref) {
+        let message = arg ? ref(arg) : ref()
+        if (message) {
+            console.log(message)
+            speak(message)
+        }
+    } else {
+        speak('no key')
+    }
     return 1
 }
-
-
-function visualHandler(s) {
+function visualHandler(s, enter) {
     let cm = e.cm
     const cursor = cmCursor(cm)
     const [a, b] = splitonce(s)
-
-    //console.log([a, b])
-    if (a in visualActions) {
-        visualActions[a](cm, cursor, b)
-    }
-    else if (a in visualRef) {
+    if (a in VisualCommands) {
+        VisualCommands[a](cm, cursor, b)
+    } else if (a in visualRef) {
         cmWrap(cm, a, b)
+    } else if (enter && LangRef[e.lang]?.visualHandler) {
+        LangRef[e.lang].visualHandler(cm, cursor, s)
+    } else {
+        return 0
     }
-}
-
-function jsEnter(cm) {
-    return 
+    return 1
 }
 
 class CommandTree {
@@ -127,7 +120,7 @@ class CommandTree {
         }
     }
     has(k) {
-        const value =  k in this.currentRef
+        const value = k in this.currentRef
         if (!value) {
             this.currentRef = this.store
         }
@@ -135,7 +128,6 @@ class CommandTree {
     }
     run(k, ...args) {
         this.display += k
-        console.log('run @ k', k)
         this.currentRef = this.currentRef[k]
         if (isFunction(this.currentRef)) {
             this.currentRef(...args)
@@ -167,81 +159,35 @@ function commandTreeInputFactory(commands) {
     const commandTree = new CommandTree(commands)
 
     return function inputHandler(cm, f, t, k) {
-
         if (commandTree.has(k)) {
             const line = cmLine(cm, f)
             const spaces = getIndent(line.text)
             try {
+                //console.log('doing commandtree ' + k)
                 commandTree.run(k, cm, f, line, spaces)
-            }
-            catch(e) {
+            } catch (e) {
                 console.log(e)
             }
             return 1
         } else if (commandTree.display) {
-            //console.log(commandTree.display, 'HI')
+            console.log(commandTree.display, 'HI')
             cmInsert(cm, commandTree.display + k)
             commandTree.display = ''
             return 1
         }
     }
 }
-const javascriptInputHandler = inputFactory('js')
-const htmlInputHandler = inputFactory('html')
-const cssInputHandler = inputFactory('css')
-let fnModeString = ''
-let fnModeLib = {
-    
-}
-
-function runFnModeFunction(cm, s) {
-    const createParser = (x) => {
-        let [a,b] = split(s, '  ')
-        return (s) => {
-            return replace(a, b, s, 'gm')
-        }
-    }
-    const parser = fnModeLib[s] || createParser(s)
-    cmReplace(cm, cmPage, a, parser)
-    //<(div)[]>$
-}
-
-function globalEnter(cm) {
-    if (app.normalMode || app.visualMode) {
-        speak('running normal mode')
-        let [key, arg] = splitonce(app.modeString)
-        let ref
-
-        if (app.normalMode) {
-            ref = globalNormalRefs[key]
-            if (!ref) {
-                ref = normalRef[app.editor.lang][key]
-            }
-        if (ref) {
-            ref()
-        } else {
-            speak('nox key')
-        }
-        }
-
-        else if (app.visualMode) {
-            //ref = globalVisualRefs[key]
-            //if (!ref) {
-                //ref = visualRef[app.editor.lang][key]
-            //}
-            visualHandler(app.modeString)
-        }
-
-        app.modeString = ''
-        if (app.normalMode) app.normalMode = false
-        else if (app.visualMode) app.visualMode = false
-        return 1
-    }
-    return CodeMirrorCommands.insertBlankLine(cm)
-}
 
 function inputFactory(lang) {
-    const { iabSnippets, imapRef, magicLeader, treeCommands, useLeader } = GlobalReferences[lang]
+    const {
+        imapRef,
+        magicLeader,
+    } = GlobalReferences[lang]
+
+    const treeCommands = {
+        ...TreeCommands.global,
+        ...TreeCommands[lang]
+    }
     const treeHandler = commandTreeInputFactory(treeCommands)
 
     return function lambdaInputHandler(cm, f, t, k) {
@@ -266,83 +212,21 @@ function inputFactory(lang) {
         }
 
         if (imapRef && display in imapRef) {
-            return completeOnImap(cm, f, line, display, wif, imapRef)
+            return completeOnImap(
+                cm,
+                f,
+                line,
+                display,
+                wif,
+                imapRef
+            )
         }
 
         if (treeHandler(cm, f, t, k)) {
-             return 1
+            return 1
         }
     }
 }
-
-
-function maybedepreleaderHandler(
-    cm,
-    line,
-    f,
-    match,
-    text,
-    leaderCommands
-) {
-    if (match in leaderCommands) {
-        //console.log('LC')
-        return (
-            leaderCommands[match](cm) ||
-            cmTextCompletion(cm, f, match, '')
-        )
-    }
-    if (match in lastWords) {
-        return cmSnippetCompletion(
-            cm,
-            f,
-            match.length,
-            lastWords[match]
-        )
-    }
-
-    if (getFirstWord(text) in firstWords) {
-        cmVeryMagicHandler(cm, line, text)
-        //cmInsert(cm, 'sup')
-        return 1
-    }
-
-    match = match.slice(-1)
-    if (match in leaderCommands) {
-        return (
-            leaderCommands[match](cm, match.length) ||
-            cmTextCompletion(cm, f, match, '')
-        )
-    }
-}
-
-function iijavascriptInputHandler(cm, f, t, k) {
-    let [line, text, match] = inputHandlerInfo(cm, f)
-    const display = match + k
-    const wif = f < line.to
-
-    if (wif) {
-        return console.log('wif')
-        if (k == '[') {
-            return handleBraces(cm, f)
-        }
-        if (k == '{') {
-            return handleBrackets(cm, f)
-        }
-    }
-    if (k == ' ') {
-        return completeOnSpace(cm, f, match)
-    }
-    if (k == LEADER_KEY && match in tabCommandRef) {
-        tabCommandRef[match](cm)
-        return cmTextCompletion(cm, f, match, '')
-    }
-
-    if (display in completeOnImapRef) {
-        return completeOnImap(cm, f, line, display, wif)
-    }
-}
-
- 
 
 function tabFactory(lang) {
     const {
@@ -354,7 +238,11 @@ function tabFactory(lang) {
 
     return function tabHandler(cm) {
         const [pos, match, line, wif] = cmLineInfo(cm)
-        if (match && !wif && cmWordSpiral(cm, pos, line.number, match)) {
+        if (
+            match &&
+            !wif &&
+            cmWordSpiral(cm, pos, line.number, match)
+        ) {
             return 1
         }
 
@@ -382,15 +270,14 @@ function tabFactory(lang) {
     }
 }
 
-
-function cssEscape(cm) {
-    app.cssNormalMode = true
-    app.cssNormalString = ''
-}
-
 function createLanguageRef() {
+
+    const javascriptInputHandler = inputFactory('js')
+    const htmlInputHandler = inputFactory('html')
+    const cssInputHandler = inputFactory('css')
+
     const cssKeyBindings = [
-        { key: 'Tab', run: cssTab },
+        //{ key: 'Tab', run: cssTab },
         { key: 'Enter', run: cssEnter },
         //{ key: 'Escape', run: cssEscape },
     ]
@@ -408,9 +295,9 @@ function createLanguageRef() {
     //const javascriptInputHandler = commandTreeInputFactory('js')
 
     const javascriptKeyBindings = [
-        { key: 'Tab', run: jsTab },
-        { key: 'Ctrl-Enter', run: jsEnter },
-        { key: 'Enter', run: jsEnter },
+        //{ key: 'Tab', run: jsTab },
+        //{ key: 'Ctrl-Enter', run: jsEnter },
+        //{ key: 'Enter', run: jsEnter },
     ]
 
     const languageRef = {
@@ -448,12 +335,10 @@ function inputHandlerInfo(cm, f) {
     return [line, text, match]
 }
 
-
 function completeOnSpace(cm, f, key) {
     const word = completeOnSpaceRef[key]
     return cmTextCompletion(cm, f, key, word)
 }
-
 
 function completeOnImap(cm, f, line, display, wif, ref) {
     const offset = display.length - 1
@@ -478,7 +363,7 @@ function completeOnImap(cm, f, line, display, wif, ref) {
             })
             return 1
         }
-        return 
+        return
     }
 
     if (isFunction(word)) {
@@ -492,13 +377,20 @@ function completeOnImap(cm, f, line, display, wif, ref) {
         return 1
     }
 
-
     if (word.includes('$')) {
-        return cmDollar(cm, line, word, spaces, f - display.length + 1, 'spicy!')
+        return cmDollar(
+            cm,
+            line,
+            word,
+            spaces,
+            f - display.length + 1,
+            'spicy!'
+        )
     }
 
     const parenOffset = getParenOffset(word)
-    return cmDispatchText(cm,
+    return cmDispatchText(
+        cm,
         f - offset,
         f,
         word,
@@ -506,56 +398,18 @@ function completeOnImap(cm, f, line, display, wif, ref) {
     )
 }
 
-
-function handleBrackets(cm, f) {
-    let tree = syntaxTree(cm.state)
-    let next = tree.resolveInner(f, 1)
-    cm.dispatch({
-        changes: [
-            { from: f, to: f, insert: '[' },
-            { from: next.to, to: next.to, insert: ']' },
-        ],
-        selection: {
-            anchor: next.to + next.to - next.from,
-        },
-    })
-    return 1
-}
-
-
-const vimCommandTree = new CommandTree(VimKeyBindingRef)
-
-function vimInputHandler(cm, f, t, k) {
-    console.log(vimCommandTree.display, 'display')
-    //console.log('hi from vim input handler', k)
-    if (vimCommandTree.has(k)) {
-        //console.log('has it', k)
-        const line = cmLine(cm, f)
-        const spaces = getIndent(line.text)
-        vimCommandTree.run(k, cm, f, line, spaces)
-    } else {
-        vimCommandTree.display = ''
-    }
-    return 1
-}
-function handleNormalMode(s) {
-    let ref = globalNormalRefs[s] || normalRef[e.lang][s]
-    //console.log(Object.keys(normalRef[e.lang]))
-    //console.log(s, ref)
-    if (ref) {
-        ref()
-        app.normalMode = false
-        app.modeString = ''
-    }
-}
 function baseInputHandler(cm, f, t, k) {
     if (app.normalMode || app.visualMode) {
         app.modeString += k
         if (app.modeString.includes(' ')) return 1
-        if (app.visualMode) {
-            visualHandler(app.modeString)
-        } else {
-            handleNormalMode(app.modeString)
+        let result = app.visualMode
+            ? visualHandler(app.modeString)
+            : normalHandler(app.modeString)
+
+        if (result) {
+            app.modeString = ''
+            app.visualMode = false
+            app.normalMode = false
         }
         return 1
     }
@@ -564,18 +418,100 @@ function baseInputHandler(cm, f, t, k) {
 function globalEscape() {
     if (app.visualMode) {
         app.visualMode = false
-        app.modeString = ''
-        return 1
+    } else {
+        console.log({o:opposite(app.normalMode)})
+        app.normalMode = opposite(app.normalMode)
     }
-    
-    app.normalMode = opposite(app.normalMode)
     app.modeString = ''
     return 1
 }
 
 function globalBackspace() {
     if (app.visualMode || app.normalMode) {
-    app.modeString = backspaced(app.modeString)
-    return 1
+        app.modeString = backspaced(app.modeString)
+        return 1
     }
+}
+
+function globalEnter(cm) {
+    if (app.normalMode || app.visualMode) {
+        if (app.normalMode) {
+            normalHandler(app.modeString, true)
+        } else if (app.visualMode) {
+            visualHandler(app.modeString, true)
+        }
+
+        app.modeString = ''
+        if (app.normalMode) app.normalMode = false
+        else if (app.visualMode) app.visualMode = false
+        return 1
+    }
+    if (e.lang == 'js') return 
+    return CodeMirrorCommands.insertBlankLine(cm)
+}
+
+function cmVeryMagicHandler(cm, line) {
+
+    if (/[^\s\w]/.test(line.text)) return 
+    let [spaces, text] = getIndentAndLine(line.text)
+    const [tag, rest] = splitonce(text)
+
+    let template = firstWords[tag]
+    if (!template) {
+        return 
+    }
+
+    let n = getLongestDollar(template)
+    let args =
+        n == 2
+            ? splitOnceReverse(rest, ' ')
+            : rest
+            ? [rest]
+            : []
+
+    if (args.length < n) {
+        let lastBinding = cmGetLastBinding(cm, line)
+        args.push(lastBinding)
+    }
+
+    template = indent(template, spaces)
+    let chunk = spicyTemplater(template, args)
+    const [insert, cursor] = getCursorFromText(chunk)
+    return cmDispatchText(
+        cm,
+        line.from,
+        line.to,
+        insert,
+        cursor + line.from, 
+    )
+}
+
+/* the magic handler is indeed an input handler */
+/* changing things around without stable saves is dangerous */
+
+
+function visualHandlerHTML(cm, cursor, s) {
+    const node = cmNode(cm)
+
+    if (node.name == 'Text') {
+        cmReplace(cm, node, s)
+    }
+    else if (node.name == 'TagName') {
+        let div = cm.state.sliceDoc(node.from, node.to)
+        cmReplace(cm, cmLine, boundary(div), s)
+    }
+}
+
+/* that is who he is ... we choose people who make us feel good */
+/*  */
+
+function globalTab(cm, f, t, k) {
+
+    const [pos, match, line, wif] = cmLineInfo(cm)
+    if (match && cmWordSpiral(cm, pos, line.number, match)) {
+        return 1
+    }
+
+    cm.dispatch(cm.state.replaceSelection('\t'))
+    return 1
 }
